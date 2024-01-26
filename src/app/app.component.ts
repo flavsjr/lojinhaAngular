184e87e105f7720512b910cc0c5b15e14e13fbdf
flavsjr <flavianogjr@gmail.com>
2024-01-26 16:20:20 -0300

    Iniciando mudanças  básicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Início da Loja</h1>
 
     <router-outlet />
   `,
