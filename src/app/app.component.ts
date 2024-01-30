c984de8f5b092eaeb19c1f84d1c6ece6d6fc0855
flavsjr <flavianogjr@gmail.com>
2024-01-30 13:53:09 -0300

    Ajustando components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   template: `
-
+  <app-header></app-header>
   `,
   styles: [],
 })
