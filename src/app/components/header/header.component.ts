eec950c886971ee444ff8cd8a09e9ee7509e3075
flavsjr <flavianogjr@gmail.com>
2024-02-29 13:09:22 -0300

    Adicionando carrinho de compras (icon)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularMaterialModule } from '../shared/angular-material/angular-material.module';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatBadge } from '@angular/material/badge';
+import { MatIcon } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  imports: [AngularMaterialModule]
+  imports: [
+    AngularMaterialModule,
+    MatMenuTrigger,
+    MatBadge,
+    MatIcon
+  ]
 })
 export class HeaderComponent implements OnInit {
 
